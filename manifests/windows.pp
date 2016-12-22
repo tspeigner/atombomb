e92741a9d00c54b6b164dad6ced589a0468ba8dc
Tommy Speigner <tspeigner@gmail.com>
2016-12-22 11:17:26 -0800

    changed to windows profile

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -1,7 +1,7 @@
 #atombomb
 #Install Atom on Windows.
 
-class atombomb {
+class atombomb::windows {
   
   package { 'atom':
     ensure   => latest,
@@ -14,25 +14,5 @@ class atombomb {
   }
 
    package {['linter','atom-lint','puppet-lint','git-plus','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-icons','file-type-icons','git-plus','highlight-selected','language-puppet','linter-puppet-lint','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent',  ]:}
-  
-  
- # package { 'atom-lint':
- #   ensure   => latest,
- #   provider => apm,
- #} 
- # 
- # package { 'puppet-lint':
- #   ensure   => latest,
- #   provider => apm,
- # }
- # 
- # package { 'git-plus':
- #   ensure   => latest,
- #   provider => apm,
- # }
- # 
- # package { 'language-puppet':
- #   ensure   => latest,
- #   provider => apm,
- # }
+ 
 }
