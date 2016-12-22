5a0d35a34658b4d1322867aaf4fec252e0c94f82
Tommy Speigner <tspeigner@gmail.com>
2016-12-21 16:20:16 -0800

    reconfigured things

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,27 @@
 #Install Atom on Windows.
 
 class atombomb {
+  
+  package { 'atom':
+    ensure   => latest,
+    provider => 'chocolatey',
+  }
+  
+  package { 'atom-lint':
+    ensure   => latest,
+    provider => apm,
+  } 
+  
+  package { 'puppet-lint':
+    ensure   => latest,
+    provider => apm,
+  }
+  
+  package { 'git-plus':
+    ensure   => latest,
+    provider => apm,
+  }
+  
   package { 'language-puppet':
     ensure   => latest,
     provider => apm,
