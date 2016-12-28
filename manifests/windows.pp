cc7d2a27f8b4f662b89ede381f00e90d6a845785
tspeigner <tspeigner@gmail.com>
2016-12-28 14:08:58 -0800

    removing packages

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -15,14 +15,14 @@ windows_path { 'Add a PATH entry if it is not there yet':
 
 #Install Atom on Windows.
   package { 'atom':
-    ensure   => latest,
+    ensure   => absent,
     provider => 'chocolatey',
   }
 
 #Install packages with APM
   Package{
     provider => apm,
-    ensure   => latest,
+    ensure   => absent,
   }
 
    package {['linter','atom-lint','linter-puppet','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
