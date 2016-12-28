ae9003c0d435990488846324bc5074ff751fa250
tspeigner <tspeigner@gmail.com>
2016-12-28 11:57:33 -0800

    removing comment lines for packages

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -7,12 +7,12 @@ class atombomb::windows {
     provider => 'chocolatey',
   }
 
-##Install packages with APM
-#  Package{
-#    provider => apm,
-#    ensure   => latest,
-#  }
-#
-#   package {['linter','atom-lint','linter-puppet','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
+#Install packages with APM
+  Package{
+    provider => apm,
+    ensure   => latest,
+  }
+
+   package {['linter','atom-lint','linter-puppet','language-puppet','sync-on-save','language-docker','advanced-open-file','file-icons','terminal-plus', 'aligner','aligner-puppet','atom-beautify','auto-detect-indentationb','file-type-icons','git-plus','highlight-selected','minimap','minimap-cursorline','minimap-highlight-selected','minimap-linter','minimap-pigments','minimap-selection','open-recent','project-manager' ]:}
 
 }
