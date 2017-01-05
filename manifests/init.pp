d5f786fd2445059d6b332ed902ec95cc0918da26
tspeigner <tspeigner@gmail.com>
2017-01-05 13:37:33 -0800

    type

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 class {
   case $::kernel {
       'windows': { include puppet_atom_env::windows }
-      'darwin' : { include puppet_atom_env::osx }
+      'darwin': { include puppet_atom_env::osx }
       #'darwin' : { include brew
       #  $provider_type='brew' }
     }
