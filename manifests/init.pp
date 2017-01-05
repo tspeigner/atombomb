35e56b8a60b59efdd3109ec59fc3404417c63eb6
tspeigner <tspeigner@gmail.com>
2017-01-05 13:39:30 -0800

    better?

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,8 @@ class {
   case $::kernel {
       'windows': { include puppet_atom_env::windows }
       'darwin': { include puppet_atom_env::osx }
-      #'darwin' : { include brew
-      #  $provider_type='brew' }
-    }
+  }
 }
+
+#'darwin' : { include brew
+#  $provider_type='brew' }
